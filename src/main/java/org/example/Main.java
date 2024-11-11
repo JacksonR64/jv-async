c0684c085ad2ef3def95bbeec5cf5e35c512c2f9
Jackson <jackson_rhoden@outlook.com>
2024-11-11 12:25:30 +0000

    feat: added Asycn tasks 1-4 completed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,35 +1,61 @@
 package org.example;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.function.BiFunction;
 
 public class Main {
     public static void main(String[] args) {
 
-        CompletableFuture.runAsync(() -> {
+
+
+        CompletableFuture<String> part1 = CompletableFuture.supplyAsync(() -> {
             try {
                 Thread.sleep(3000);
 
             } catch (InterruptedException e) {
                 System.out.println("error");
-                throw new RuntimeException(e);
             }
-            System.out.println("Hello...");
+            return "Hello... ";
 
         });
-        CompletableFuture.runAsync(() -> {
+        String part3 = String.valueOf(part1);
+        CompletableFuture<String> part2 = CompletableFuture.supplyAsync(() -> {
             try {
                 Thread.sleep(5000);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-            System.out.println("...world!");
+            return "...world!";
         });
+
+
+
+
+
         try {
-            Thread.sleep(10000);
+            Thread.sleep(5000);
+
         } catch (InterruptedException e) {
+
             throw new RuntimeException(e);
         }
-        System.out.println("test");
+
+        BiFunction<String, String, String> function = (value, value2) -> combineFunctionality(value, value2);
+
+
+        CompletableFuture<String> thenCombine = part1.thenCombine(part2, function);
+
+        System.out.println("output: " + thenCombine.join());
+
+
     }
+    public static String combineFunctionality(String input1, String input2) {
+        System.out.println("part 1: " + input1);
+        System.out.println("part 2: " + input2);
+
+        return input1 + input2;
+
+    }
+
 
 }
